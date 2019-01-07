853a73c0317b028257119cc9fcaa948805ccc48e
Andrew Newlands <andrewn@remasys.com>
2019-01-07 16:38:56 +1100

    - Added an warning noting that the loop over long ints does not  handle array lengths that are not divisible by 8, i.e. it does not properly handle a potential overhang at the end. - Added a check to confirm both arrays are of identical length in this test.

--- MemoryComparison/ForInt64Comparison.cs
+++ MemoryComparison/ForInt64Comparison.cs
@@ -9,8 +9,13 @@ namespace MemoryComparison
     public class ForInt64Comparison: IComparison
     {
         // The fastest pure c# version so far...
+        // N.B. this assumes that the array length is a multiple of 8.
+        // In reality, it would be necessary to handle any overhang (i.e. the trailing 0-7 bytes at the end)!
         public unsafe bool Compare(byte[]A, byte[]B)
         {
+            if (A.Length != B.Length)
+                return false;
+
             fixed( byte* pA = A, pB = B)
             {
                 long* lA = (long*)pA;
